48eebf7197b1cee56e455a130820c49525a04417
Prudhvi Gadiraju <gadiraju.prudhvi@gmail.com>
2018-10-15 23:13:59 -0700

    init commit

---
+++ PixelCity/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PixelCity
+//
+//  Created by Prudhvi Gadiraju on 10/15/18.
+//  Copyright © 2018 Kore. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
